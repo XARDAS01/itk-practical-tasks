81050cc687c676bb259a6115faeaf4783efe36bb
Yuri <yuri@MacBook-Air-Yuri.local>
2025-02-01 13:57:05 +0100

    feat: task 7

---
+++ src/main/java/academy/itk/task7/Order.java
@@ -0,0 +1,25 @@
+package academy.itk.task7;
+
+public class Order {
+
+    private String product;
+    private double cost;
+
+    public Order(String product, double cost) {
+        this.product = product;
+        this.cost = cost;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    @Override
+    public String toString() {
+        return this.product + " " + this.cost;
+    }
+}
